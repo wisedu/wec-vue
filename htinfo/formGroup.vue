d061f24698fbb8d41911f95ea25f748598977c23
dan <danzhang@wisedu.com>
2017-01-22 17:33:52 +0800

    增加title的bh-role-form-outline属性

--- htinfo/formGroup.vue
+++ htinfo/formGroup.vue
@@ -1,7 +1,7 @@
 <template>
     <div class='bh-form-horizontal'>
         <div>
-            <div class="bh-col-md-12 bh-form-groupname sc-title-borderLeft bh-mb-24" >{{title}}</div>
+            <div class="bh-col-md-12 bh-form-groupname sc-title-borderLeft bh-mb-24" bh-role-form-outline="title">{{title}}</div>
             <div class='bh-form-block bh-ph-16'>
                 <slot></slot>
             </div>
